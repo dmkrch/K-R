d49a90a2abd92a17233b695376bf0519b541c190
Dmitriy Karpovich <dmkrch1@gmail.com>
2020-06-18 10:36:58 +0300

    modified function pointer prog

--- FunctionPointer/FunctionPointer/FunctionPointer.cpp
+++ FunctionPointer/FunctionPointer/FunctionPointer.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string>
 
 
 int Sum(int a, int b)
@@ -18,9 +19,25 @@ int Calculate(int a, int b, int(*op)(int a, int b))
 
 int main()
 {
+	/* 1st example */
 	int a = 5;
 	int b = 7;
 
-	printf("\n%d + %d = %d", a, b, Calculate(a, b, Sum));
-	printf("\n%d - %d = %d", a, b, Calculate(a, b, Subtr));
+	printf("\n%d + %d = %d", a, b, Calculate(a, b, Sum));	// 12
+	printf("\n%d - %d = %d", a, b, Calculate(a, b, Subtr)); // -2
+
+	/* 2nd example */
+	int (*strcompare)(const char*, const char*);
+	strcompare = strcmp;
+
+	char* str1;
+	char* str2;
+
+	char arr1[] = "hello";
+	char arr2[] = "how";
+
+	str1 = arr1;
+	str2 = arr2;
+
+	printf("%d", (*strcompare)(str1, str2));	// -1
 }
